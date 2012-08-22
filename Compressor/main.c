e5a8a71e08d2f08754e385ef92b5166260e60741
Krzysztof Gabis <kgabis@gmail.com>
2012-08-22 22:01:19 +0200

    Initial Commit

---
+++ Compressor/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Compressor
+//
+//  Created by Krzysztof Gabis on 22.08.2012.
+//  Copyright (c) 2012 Krzysztof Gabis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
